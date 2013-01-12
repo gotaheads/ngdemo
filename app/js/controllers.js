70d4ee0519d813930948c2a6e5e405d2e0ab1d84
Hiroki Gota <gotahiroki@gmail.com>
2013-01-13 10:08:09 +1100

    'include demo'

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,22 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+function MyCtrl1($scope) {
+}
+
+MyCtrl1.$inject = ['$scope'];
+
+function TemplateCtrl($scope) {
+
+  $scope.templates =
+    [ { name: 'partial1.html', url: 'partials/partial1.html'},
+      { name: 'partial2.html', url: 'partials/partial2.html'},
+      { name: 'partial3.html', url: 'partials/partial3.html'}];
+
+  $scope.template = $scope.templates[0];	
+}
+
+TemplateCtrl.$inject = ['$scope'];
 
 
 function MyCtrl2() {
